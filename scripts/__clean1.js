e4ee73f8b232fe3be54cda07fa57a43b4800c3a5
SongerJia <35770363+zz12121@users.noreply.github.com>
2026-08-09 09:23:07 +0800

    chore: 移除误提交的临时清理脚本

--- scripts/__clean1.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-let s = fs.readFileSync('src/components/Profile.vue', 'utf8')
-const before = s
-s = s.replace(/^import XpDetailModal[^\n]*\r?\n/gm, '')
-s = s.replace(/^const showXpDetail[^\n]*\r?\n/gm, '')
-s = s.replace(/^\s*<XpDetailModal[^\n]*\r?\n/gm, '')
-if (s === before) { console.log('NO CHANGE'); process.exit(1) }
-fs.writeFileSync('src/components/Profile.vue', s)
-console.log('removed. remaining refs:', (s.match(/XpDetailModal|showXpDetail/g) || []).length)
